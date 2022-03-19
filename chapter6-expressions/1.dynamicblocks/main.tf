60d8a83dce49af438a1375aa4d1af3b154e8bcfe
sunil kumar <sunilyadav0201@live.com>
2022-03-19 06:55:20 +0000

    adding aws security group using dynmaic blocks

--- chapter6-expressions/1.dynamicblocks/main.tf
+++ chapter6-expressions/1.dynamicblocks/main.tf
@@ -9,4 +9,58 @@ terraform {
 
 provider "aws" {
   # Configuration options
+  profile = "default"
+  region = "us-east-1"
+}
+
+data "aws_vpc" "main" {
+    id="vpc-0515341caa2b88c6c"
+  
+}
+
+
+locals {
+  ingress= [{
+      port=443
+      description= "port 443"
+      protocol= "tcp"
+  }]
+}
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  description = "allow tls inbound traffic"
+  vpc_id=data.aws_vpc.main.id
+
+    dynamic "ingress"{
+        for_each=local.ingress
+        content {
+            description      = ingress.value.description
+            from_port        = ingress.value.port
+            to_port          = ingress.value.port
+            protocol         = ingress.value.protocol
+            cidr_blocks      = [data.aws_vpc.main.cidr_block]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self= false
+        }
+
+    }
+
+  egress {
+    description= "outgoing for all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self= false
+
+  }
+
+  tags = {
+    Name = "allow_tls traffic"
+  }
 }
